cf055572cc63e61b3631d2fbf3b57384ba399804
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-20 21:40:53 -0700

    fixed errors on unused variables and added margins

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,7 +4,7 @@ import { GlobalContext } from '../context/GlobalState';
 export const AddTransaction = () => {
 	const [text, setText] = useState('');
 	const [amount, setAmount] = useState(0);
-	const [expense, setExpense] = useState(false);
+	// const [expense, setExpense] = useState(false);
 
 	const { addTransaction } = useContext(GlobalContext);
 
